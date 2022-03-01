829d1aaf1c36ee11ea2e7eb6f87aeec2770b6f91
Miller <farwes1180@gmail.com>
2022-03-02 08:14:17 +0900

    Registering a Screen as the Main Screen;

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DeliMeals'),
-      ),
-      body: const Center(
-        child: Text('MEALSAPP'),
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
