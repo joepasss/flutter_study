46ed22de53fe2c6fef76db5f9742a8f210ef7542
Miller <farwes1180@gmail.com>
2022-03-06 16:48:30 +0900

    Add Named routes & passing data from args

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,17 +1,20 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   const CategoryMealsScreen({
     Key? key,
-    required this.categoryId,
-    required this.categoryTitle,
   }) : super(key: key);
-
-  final categoryId;
-  final categoryTitle;
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+
+    final String categoryTitle = routeArgs['title'].toString();
+    final String categortId = routeArgs['id'].toString();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +35,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: const CategoriesScreen(),
+      routes: {
+        '/category-meals': (context) => CategoryMealsScreen(),
+      },
     );
   }
 }
