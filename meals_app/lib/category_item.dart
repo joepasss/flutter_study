e8c87ef2d24790e51dbe955d0a382ba752776236
Miller <farwes1180@gmail.com>
2022-03-10 11:32:34 +0900

    Add global routeName

--- meals_app/lib/category_item.dart
+++ meals_app/lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem({
@@ -13,10 +14,13 @@ class CategoryItem extends StatelessWidget {
   final Color color;
 
   void selectCategory(BuildContext context) {
-    Navigator.of(context).pushNamed('/category-meals', arguments: {
-      'id': id,
-      'title': title,
-    });
+    Navigator.of(context).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -34,9 +34,10 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: const CategoriesScreen(),
+      initialRoute: '/',
       routes: {
-        '/category-meals': (context) => const CategoryMealsScreen(),
+        '/': (context) => const CategoriesScreen(),
+        CategoryMealsScreen.routeName: (context) => const CategoryMealsScreen(),
       },
     );
   }
